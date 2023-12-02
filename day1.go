45b4f011aa611cb1f36e3eafa0a8fac9583e9ad2
rossola <rossola@tecniplast.it>
2023-12-02 01:07:27 +0100

    part 2

--- day1.go
+++ day1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -18,6 +19,16 @@ const (
 	seven = "seven"
 	eight = "eight"
 	nine  = "nine"
+
+	eno   = "eno"
+	owt   = "owt"
+	eerht = "eerht"
+	ruof  = "ruof"
+	evif  = "evif"
+	xis   = "xis"
+	neves = "neves"
+	thgie = "thgie"
+	enin  = "enin"
 )
 
 func main() {
@@ -26,14 +37,209 @@ func main() {
 		panic(err)
 	}
 
-	part1(lines)
+	// part1(lines)
 
 	part2(lines)
 
 }
 
-func part2(lines []string) {
+func part2(lines []string) int {
+	var partTwoTotal int = 0
+	for i := 0; i < len(lines); i++ {
+		fmt.Println("LINE:", lines[i])
+		var isFirstDigitFound bool = false
+		var firstNumber string = ""
+		var maybeFirstStringDigit string = ""
+		for _, char := range lines[i] {
+			if unicode.IsDigit(char) && !isFirstDigitFound {
+				isFirstDigitFound = true
+				firstNumber = string(char)
+				fmt.Printf("firstNumber: %v\n", firstNumber)
+				maybeFirstStringDigit = ""
+				break
+			}
+			if unicode.IsLetter(char) && !isFirstDigitFound {
 
+				maybeFirstStringDigit += string(char)
+
+				num, _ := regexToNumber(maybeFirstStringDigit, false)
+				if num != "" {
+					fmt.Println("First number as string: ", num)
+					maybeFirstStringDigit = num
+					isFirstDigitFound = true
+					firstNumber = ""
+					break
+				}
+			}
+		}
+
+		reversedString := reverseString(lines[i])
+
+		var isLastDigitFound bool = false
+		var lastNumber string = ""
+		var maybeLastStringDigit string = ""
+		for _, char := range reversedString {
+			if unicode.IsDigit(char) && !isLastDigitFound {
+				isLastDigitFound = true
+				lastNumber = string(char)
+				fmt.Printf("lastNumber: %v\n", lastNumber)
+				maybeLastStringDigit = ""
+				break
+			}
+			if unicode.IsLetter(char) && !isLastDigitFound {
+				maybeLastStringDigit += string(char)
+
+				num, _ := regexToNumber(maybeLastStringDigit, true)
+				if num != "" {
+					fmt.Println("Last number as string: ", num)
+					maybeLastStringDigit = num
+					isLastDigitFound = true
+					lastNumber = ""
+					break
+				}
+			}
+		}
+
+		var firstNumberAsString string = ""
+		var lastNumberAsString string = ""
+		if "" != firstNumber {
+			firstNumberAsString = firstNumber
+		} else if "" != maybeFirstStringDigit {
+			firstNumberAsString, _ = letterToStringNumber(maybeFirstStringDigit)
+		}
+		if "" != lastNumber {
+			lastNumberAsString = lastNumber
+		} else if "" != maybeLastStringDigit {
+			lastNumberAsString, _ = letterToStringNumber(maybeLastStringDigit)
+		}
+		lineTotal := firstNumberAsString + lastNumberAsString
+		lineDigitsAsInt, _ := strconv.Atoi(lineTotal)
+		fmt.Printf("line calibration: %v\n", lineDigitsAsInt)
+		partTwoTotal += lineDigitsAsInt
+	}
+	fmt.Printf("Part Two Total: %v\n", partTwoTotal)
+	return partTwoTotal
+}
+
+func regexToNumber(s string, isReverse bool) (string, error) {
+	// Convert the input string to lowercase for case-insensitive comparison
+	lowercaseInput := strings.ToLower(s)
+
+	// Map of number words to their corresponding integers
+	var numberList []string
+	numberList = append(numberList,
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	)
+
+	var numberListReverse []string
+	numberListReverse = append(numberList,
+		"eno",
+		"owt",
+		"eerht",
+		"ruof",
+		"evif",
+		"xis",
+		"neves",
+		"thgie",
+		"enin",
+	)
+
+	// Iterate over the map to find a match in the input string
+	if isReverse {
+		for _, word := range numberListReverse {
+			if strings.Contains(lowercaseInput, word) {
+				return word, nil
+			}
+		}
+	} else {
+		for _, word := range numberList {
+			if strings.Contains(lowercaseInput, word) {
+				return word, nil
+			}
+		}
+	}
+
+	// If no match is found, return an error
+	return "", fmt.Errorf("unsupported string: %s", s)
+}
+
+func stringToInt(s string) (int, error) {
+	// Convert the input string to lowercase for case-insensitive comparison
+	lowercaseInput := strings.ToLower(s)
+
+	// Map of number words to their corresponding integers
+	numberMapReverse := map[string]int{
+		"eno":   1,
+		"owt":   2,
+		"eerht": 3,
+		"ruof":  4,
+		"evif":  5,
+		"xis":   6,
+		"neves": 7,
+		"thgie": 8,
+		"enin":  9,
+	}
+
+	// Iterate over the map to find a match in the input string
+	for word, value := range numberMapReverse {
+		if strings.Contains(lowercaseInput, word) {
+			return value, nil
+		}
+	}
+
+	// If no match is found, return an error
+	return 0, fmt.Errorf("unsupported string: %s", s)
+}
+
+func letterToStringNumber(s string) (string, error) {
+	switch strings.ToLower(s) {
+	case "one":
+		return "1", nil
+	case "two":
+		return "2", nil
+	case "three":
+		return "3", nil
+	case "four":
+		return "4", nil
+	case "five":
+		return "5", nil
+	case "six":
+		return "6", nil
+	case "seven":
+		return "7", nil
+	case "eight":
+		return "8", nil
+	case "nine":
+		return "9", nil
+	case "eno":
+		return "1", nil
+	case "owt":
+		return "2", nil
+	case "eerht":
+		return "3", nil
+	case "ruof":
+		return "4", nil
+	case "evif":
+		return "5", nil
+	case "xis":
+		return "6", nil
+	case "neves":
+		return "7", nil
+	case "thgie":
+		return "8", nil
+	case "enin":
+		return "9", nil
+	default:
+		return "0", fmt.Errorf("unsupported string: %s", s)
+	}
 }
 
 func part1(lines []string) int {
